eb597b706820ab93d47004f11db25f043b131a99
thermatk <thermatk@thermatk.com>
2022-09-22 00:50:52 +0200

    remove overlay for certain files

--- app/src/main/java/org/jellyfin/androidtv/ui/playback/CustomPlaybackOverlayFragment.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/playback/CustomPlaybackOverlayFragment.java
@@ -1293,7 +1293,14 @@ public class CustomPlaybackOverlayFragment extends Fragment implements LiveTvGui
                 binding.itemTitle.setText(current.getSeriesName());
                 binding.itemSubtitle.setText(BaseItemExtensionsKt.getDisplayName(ModelCompat.asSdk(current), requireContext()));
             } else {
-                binding.itemTitle.setText(current.getName());
+                if (current.getName().contains("introTFH")) {
+                    hide();
+                    leanbackOverlayFragment.setShouldShowOverlay(false);
+                    hidePopupPanel();
+                    leanbackOverlayFragment.hideOverlay();
+                } else {
+                    binding.itemTitle.setText(current.getName());
+                }
             }
             // Update the logo
             String imageUrl = ImageUtils.getLogoImageUrl(current, 440, false);
